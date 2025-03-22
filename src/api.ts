2db6dcc1e694a8d607c03e85d8cce5cfaddb7ddc
Victor Gabriel <victorgabriel08fs@gmail.com>
2025-03-21 23:26:27 -0300

    ignora env

--- src/api.ts
+++ src/api.ts
@@ -2,10 +2,9 @@ import dotenv from "dotenv";
 import axios from "axios";
 
 let api = null;
-const API_URL = "/api";
 if (import.meta.env.VITE_API_URL) {
   api = axios.create({
-    baseURL: API_URL,
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
       "Content-Type": "application/json",
     },
