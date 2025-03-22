ee224463f05330dce765dfcb46aada3cb4ae43ad
Victor Gabriel <victorgabriel08fs@gmail.com>
2025-03-21 23:20:03 -0300

    resolve http

--- src/api.ts
+++ src/api.ts
@@ -2,9 +2,10 @@ import dotenv from "dotenv";
 import axios from "axios";
 
 let api = null;
+const API_URL = "/api";
 if (import.meta.env.VITE_API_URL) {
   api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: API_URL,
     headers: {
       "Content-Type": "application/json",
     },
