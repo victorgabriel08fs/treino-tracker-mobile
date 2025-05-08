64b25ec373cb5d6f687bb00528a8addd19f955fd
Victor Gabriel <victorgabriel08fs@gmail.com>
2025-05-08 19:17:01 -0300

    fix: remove line-camp dependence

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -125,5 +125,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/line-clamp")],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
