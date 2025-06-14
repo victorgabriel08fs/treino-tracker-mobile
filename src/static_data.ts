c31c6157ba93ccbec439eaa0c07d8b8b83ed67ca
Victor Gabriel <victorgabriel08fs@gmail.com>
2025-06-14 16:02:03 -0300

    feat: add exercise names on static_data

--- src/static_data.ts
+++ src/static_data.ts
@@ -48,3 +48,162 @@ export const muscleGroups = [
     name: "Antebraço",
   },
 ];
+
+
+export const exerciseNames = [
+  {
+    "id": "1f8a9e4c-57b8-4e1d-af4e-0a1daa8d0a45",
+    "name": "Supino reto",
+  },
+  {
+    "id": "a3b3e5bf-0e22-4f39-8c1c-8b1f76a7b6a5",
+    "name": "Supino inclinado com barra",
+  },
+  {
+    "id": "3d3e0b5f-1ea5-4a15-8c8e-1b4f5f1cd8a3",
+    "name": "Crucifixo reto com halteres",
+  },
+
+  {
+    "id": "d7c0b5f9-ff0f-4f11-bb4f-4dcab4ad9c7c",
+    "name": "Puxada frontal",
+  },
+  {
+    "id": "6a88b13c-8b0f-4f5c-9a3f-7f9ebc76f2c1",
+    "name": "Remada curvada",
+  },
+  {
+    "id": "f1f9a9f2-2f01-4ba3-bb53-7b9c9b3f4f6d",
+    "name": "Levantamento terra",
+  },
+
+  {
+    "id": "3c9ab1c4-7f4f-4c5e-9df4-9b0a8b9c0d5d",
+    "name": "Rosca direta",
+  },
+  {
+    "id": "a6b7c9d1-6eef-4fb7-9b05-9b8b5e9f8f5f",
+    "name": "Rosca alternada com halteres",
+  },
+  {
+    "id": "f6f1d9eb-5fda-4d8e-99c6-7f5e3f5e0e0e",
+    "name": "Rosca martelo",
+  },
+
+  {
+    "id": "5f5f5f5f-5f5f-5f5f-5f5f-5f5f5f5f5f5",
+    "name": "Tríceps pulley",
+  },
+  {
+    "id": "8b8b8b8b-8b8b-8b8b-8b8b-8b8b8b8b8b8b",
+    "name": "Tríceps francês",
+  },
+  {
+    "id": "9c9c9c9c-9c9c-9c9c-9c9c-9c9c9c9c9c9c",
+    "name": "Tríceps banco",
+  },
+
+  {
+    "id": "5e5e5e5e-5e5e-5e5e-5e5e-5e5e5e5e5e5e",
+    "name": "Agachamento livre",
+  },
+  {
+    "id": "7f7f7f7f-7f7f-7f7f-7f7f-7f7f7f7f7f7f",
+    "name": "Cadeira extensora",
+  },
+  {
+    "id": "2a2a2a2a-2a2a-2a2a-2a2a-2a2a2a2a2a2a",
+    "name": "Leg press",
+  },
+
+  {
+    "id": "3b3b3b3b-3b3b-3b3b-3b3b-3b3b3b3b3b3b",
+    "name": "Avanço com halteres",
+  },
+  {
+    "id": "5c5c5c5c-5c5c-5c5c-5c5c-5c5c5c5c5c5c",
+    "name": "Cadeira abdutora",
+  },
+  {
+    "id": "9f9f9f9f-9f9f-9f9f-9f9f-9f9f9f9f9f9f",
+    "name": "Glúteo no cabo",
+  },
+
+  {
+    "id": "1c1c1c1c-1c1c-1c1c-1c1c-1c1c1c1c1c1c",
+    "name": "Elevação em pé no gêmeo",
+  },
+  {
+    "id": "4d4d4d4d-4d4d-4d4d-4d4d-4d4d4d4d4d4d",
+    "name": "Elevação sentado no gêmeo",
+  },
+  {
+    "id": "8e8e8e8e-8e8e-8e8e-8e8e-8e8e8e8e8e8e",
+    "name": "Gêmeos no leg press",
+  },
+
+  {
+    "id": "2e2e2e2e-2e2e-2e2e-2e2e-2e2e2e2e2e2e",
+    "name": "Desenvolvimento com halteres",
+  },
+  {
+    "id": "6c6c6c6c-6c6c-6c6c-6c6c-6c6c6c6c6c6c",
+    "name": "Elevação lateral",
+  },
+  {
+    "id": "aeaeaeae-aeae-aeae-aeae-aeaeaeaeaeae",
+    "name": "Desenvolvimento militar",
+  },
+
+  {
+    "id": "e5e5e5e5-e5e5-e5e5-e5e5-e5e5e5e5e5e",
+    "name": "Mesa flexora",
+  },
+  {
+    "id": "bebebebe-bebe-bebe-bebe-bebebebebebe",
+    "name": "Stiff com halteres",
+  },
+  {
+    "id": "f7c9a3c4-8b5f-4d3e-bb5d-6f5c8b9a0d7e",
+    "name": "Cadeira flexora",
+  },
+
+  {
+    "id": "4e5a6b7c-8d9e-4f1b-9c0d-2a3b4c5d6e7f",
+    "name": "Encolhimento com barra",
+  },
+  {
+    "id": "1a2b3c4d-5e6f-7a8b-9c0d-1e2f3a4b5c6d",
+    "name": "Encolhimento com halteres",
+  },
+  {
+    "id": "9c8b7a6d-5e4f-3a2b-1c0d-9e8f7a6b5c4d",
+    "name": "Remada alta",
+  },
+
+  {
+    "id": "7f8e9d0c-1b2a-3c4d-5e6f-7a8b9c0d1e2f",
+    "name": "Abdominal supra",
+  },
+  {
+    "id": "6d5c4b3a-2e1f-0d9c-8b7a-6e5f4d3c2b1a",
+    "name": "Prancha",
+  },
+  {
+    "id": "0d1c2b3a-4e5f-6a7b-8c9d-0e1f2a3b4c5d",
+    "name": "Abdominal com carga",
+  },
+
+  {
+    "id": "5c4d3a2b-1e0f-9d8c-7b6a-5f4e3d2c1b0a",
+    "name": "Rosca inversa",
+  },
+  {
+    "id": "8b7a6d5c-4f3e-2a1b-0c9d-8e7f6a5b4c3d",
+    "name": "Flexão de punho com barra",
+  },
+  {
+    "id": "1f2e3d4c-5b6a-7b8c-9d0e-1a2b3c4d5e6f",
+    "name": "Extensão de punho com barra",
+  }
+]
